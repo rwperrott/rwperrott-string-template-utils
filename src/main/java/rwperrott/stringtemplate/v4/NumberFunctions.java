de4dc15c3f5f4f84ce1efad4e6b94dc547a31b99
Richard Perrott <52632091+rwperrott@users.noreply.github.com>
2020-12-13 04:21:55 +0000

    Added more NumberFunctions and added aliases.

--- src/main/java/rwperrott/stringtemplate/v4/NumberFunctions.java
+++ src/main/java/rwperrott/stringtemplate/v4/NumberFunctions.java
@@ -17,64 +17,128 @@ public class NumberFunctions {
     }
 
     //
-    // Aliases for Math functions
+    // int Math functions
     //
     public static int add(int v1, int v2) {
         return Math.addExact(v1, v2);
     }
 
-    public static long add(long v1, long v2) {
-        return Math.addExact(v1, v2);
+    public static int and(int v1, int v2) {
+        return v1 & v2;
     }
 
     public static int dec(int v1) {
         return Math.decrementExact(v1);
     }
 
-    public static long dec(long v1) {
-        return Math.decrementExact(v1);
+    public static int div(int v1, int v2) {
+        return v1 / v2;
     }
 
     public static int inc(int v1) {
         return Math.incrementExact(v1);
     }
 
-    public static long inc(long v1) {
-        return Math.incrementExact(v1);
-    }
-
-    public static long sub(int v1, int v2) {
-        return Math.subtractExact(v1, v2);
-    }
-
-    public static long sub(long v1, long v2) {
-        return Math.subtractExact(v1, v2);
+    public static int mod(int v1, int v2) {
+        return v1 % v2;
     }
 
     public static int mul(int v1, int v2) {
         return Math.multiplyExact(v1, v2);
     }
 
-    public static long mul(long v1, long v2) {
-        return Math.multiplyExact(v1, v2);
+    public static int negate(int v) {
+        return -v;
+    }
+
+    public static int not(int v) {
+        return ~v;
+    }
+
+    public static int or(int v1, int v2) {
+        return v1 | v2;
+    }
+
+    public static int shl(int v1, int v2) {
+        return v1 << v2;
+    }
+
+    public static int shr(int v1, int v2) {
+        return v1 >> v2;
+    }
+
+    public static int shru(int v1, int v2) {
+        return v1 >>> v2;
+    }
+
+    public static int sub(int v1, int v2) {
+        return Math.subtractExact(v1, v2);
+    }
+
+    public static int xor(int v1, int v2) {
+        return v1 ^ v2;
     }
 
     //
-    // Added function
+    // long Math functions
     //
-    public static int div(int v1, int v2) {
-        return v1 / v2;
+    public static long add(long v1, long v2) {
+        return Math.addExact(v1, v2);
+    }
+
+    public static long and(long v1, long v2) {
+        return v1 & v2;
+    }
+
+    public static long dec(long v1) {
+        return Math.decrementExact(v1);
     }
 
     public static long div(long v1, long v2) {
         return v1 / v2;
     }
 
-    public static int mod(int v1, int v2) {
-        return v1 % v2;
+    public static long inc(long v1) {
+        return Math.incrementExact(v1);
     }
 
     public static long mod(long v1, long v2) {
         return v1 % v2;
+    }
+
+    public static long mul(long v1, long v2) {
+        return Math.multiplyExact(v1, v2);
+    }
+
+    public static long neg(long v) {
+        return -v;
+    }
+
+    public static long not(long v) {
+        return ~v;
+    }
+
+    public static long or(long v1, long v2) {
+        return v1 | v2;
+    }
+
+    public static long shl(long v1, int v2) {
+        return v1 << v2;
+    }
+
+    public static long shr(long v1, int v2) {
+        return v1 >> v2;
+    }
+
+    public static long shru(long v1, int v2) {
+        return v1 >>> v2;
+    }
+
+    public static long sub(long v1, long v2) {
+        return Math.subtractExact(v1, v2);
+    }
+
+    public static long xor(long v1, long v2) {
+        return v1 ^ v2;
     }
 }
--- src/main/java/rwperrott/stringtemplate/v4/NumberInvokeAdaptor.java
+++ src/main/java/rwperrott/stringtemplate/v4/NumberInvokeAdaptor.java
@@ -16,6 +16,29 @@ public final class NumberInvokeAdaptor extends AbstractInvokeAdaptor<Number> {
         super(true); // Probably not a good idea to access non-public Members of a Number
     }
 
+
+    @Override
+    protected String toAlias(final String name) {
+        switch (name) {
+            case "decrement":
+                return "dec"; // to StringFunctions::escapeURL
+            case "divide":
+                return "div"; // to StringFunctions::escapeURL
+            case "increment":
+                return "inc"; // to StringFunctions::escapeURL
+            case "modulus":
+                return "mod"; // to StringFunctions::escapeURL
+            case "multiply":
+                return "mul"; // to StringFunctions::escapeURL
+            case "negate":
+                return "neg"; // to StringFunctions::escapeURL
+            case "subtract":
+                return "sub"; // to StringFunctions::escapeURL
+            default:
+                return name;
+        }
+    }
+
     static {
         NumberFunctions.registerNumberAdapterFunctions();
     }
