9a42bfc96b7e42b45a364f248232bad7a0ad3e4f
Richard Perrott <52632091+rwperrott@users.noreply.github.com>
2020-12-18 21:41:51 +0000

    Added classCache, and added pre-load of some classes and package prefixes, so that other users don't have to. Added lombok.NonNull Fixed issues with getClass and getInstance

--- src/main/java/rwperrott/stringtemplate/v4/STContext.java
+++ src/main/java/rwperrott/stringtemplate/v4/STContext.java
@@ -2,6 +2,7 @@ package rwperrott.stringtemplate.v4;
 
 import it.unimi.dsi.fastutil.objects.Object2IntLinkedOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import lombok.NonNull;
 import org.stringtemplate.v4.*;
 import org.stringtemplate.v4.misc.STMessage;
 import org.stringtemplate.v4.misc.STRuntimeMessage;
@@ -16,6 +17,7 @@ import java.util.function.BiConsumer;
 import java.util.regex.Matcher;
 
 import static java.lang.String.format;
+import static rwperrott.stringtemplate.v4.Utils.fmt;
 
 /**
  * Contains lazy cache maps for start line numbers of templates in files, and for class name to Class lookup.
@@ -27,8 +29,8 @@ import static java.lang.String.format;
  */
 @SuppressWarnings("unused")
 public class STContext implements Closeable {
+    public final BiConsumer<String, Throwable> errorLog;
     private final Map<Object, Object2IntMap<String>> stGroupMetaMap = new HashMap<>();
-    private final BiConsumer<String, Throwable> errorLog;
     private final ClassLoader classLoader;
     private final Map<String, Class<?>> classCache = new HashMap<>();
 
@@ -36,13 +38,34 @@ public class STContext implements Closeable {
         this(Thread.currentThread().getContextClassLoader(), errorLog);
     }
 
-    public STContext(final ClassLoader classLoader,
-                     final BiConsumer<String, Throwable> errorLog) {
-        Objects.requireNonNull(classLoader, "classLoader");
-        Objects.requireNonNull(errorLog, "errorLog");
-
+    public STContext(final @NonNull ClassLoader classLoader,
+                     final @NonNull BiConsumer<String, Throwable> errorLog) {
         this.classLoader = classLoader;
         this.errorLog = errorLog;
+        for (Class<?> cls : PRE_CACHED_CLASSES)
+            addClass0(cls);
+    }
+
+    private void addClass0(Class<?> cls) {
+        final String className = cls.getName();
+        cacheClass(className, cls, className.lastIndexOf('.'));
+    }
+
+    private void cacheClass(String className, Class<?> cls, int p) {
+        classCache.put(className, cls);
+        final String packagePrefix = className.substring(p + 1);
+        if (PACKAGE_NAMES.contains(className.substring(p + 1)))
+            classCache.put(className.substring(p + 1), cls);
+    }
+
+    /**
+     * Should be called after all addPackage calls.
+     * @param cls a class to register
+     */
+    public void addClass(Class<?> cls) {
+        synchronized (classCache) {
+            addClass0(cls);
+        }
     }
 
     @SuppressWarnings("RedundantThrows")
@@ -50,12 +73,6 @@ public class STContext implements Closeable {
     public void close() throws IOException {
         synchronized (stGroupMetaMap) {
             stGroupMetaMap.clear();
-        }
-        synchronized (classCache) {
-            classCache.clear();
-        }
-        synchronized (supportedPackagePrefixes) {
-            supportedPackagePrefixes.clear();
         }
     }
 
@@ -67,13 +84,10 @@ public class STContext implements Closeable {
     }
 
     private <T> void registerAttributePlugins(
-            final STGroup stGroup,
-            final Map<String, String> map,
-            final Class<T> pluginType,
-            final BiConsumer<Class<?>, T> registerer) {
-        Objects.requireNonNull(stGroup, "stGroup");
-        Objects.requireNonNull(pluginType, "pluginType");
-        Objects.requireNonNull(registerer, "registerer");
+            final @NonNull STGroup stGroup,
+            final @NonNull Map<String, String> map,
+            final @NonNull Class<T> pluginType,
+            final @NonNull BiConsumer<Class<?>, T> registerer) {
         if (null == map) {
             return;
         }
@@ -81,14 +95,7 @@ public class STContext implements Closeable {
         for (Map.Entry<String, String> e : map.entrySet()) {
             final Class<?> keyType = getClass("attributeType", e.getKey(), Object.class);
             final T valueInstance = instanceOf(pluginType.getSimpleName(), e.getValue(), pluginType);
-            if (null != keyType && null != valueInstance) {
-                registerer.accept(keyType, valueInstance);
-            } else {
-                failed = true;
-            }
-        }
-        if (failed) {
-            throw new IllegalArgumentException(format("Failed to register all of the %ss", pluginType));
+            registerer.accept(keyType, valueInstance);
         }
     }
 
@@ -105,39 +112,40 @@ public class STContext implements Closeable {
             // Expand simple names for java.lang package
             final int p = className.lastIndexOf('.');
             if (-1 == p) {
-                for (String packagePrefix : supportedPackagePrefixes)
+                Exception fail = null;
+                for (String packageName : PACKAGE_NAMES) {
+                    final String key = fmt().concat(packageName,".",className);
                     try {
-                        final String key = packagePrefix + className;
-                        cls = classLoader.loadClass(key); // With default ClassLoader
+                        cls = classLoader.loadClass(key);
                         if (!type.isAssignableFrom(cls)) {
-                            errorLog.accept(format("%s '%s' not an instanceof %s",
-                                                   label, className, type.getTypeName()), null);
-                            cls = null;
+                            throw new IllegalArgumentException(format("%s '%s' not an instanceof %s",
+                                                                      label, className, type.getTypeName()), null);
                         }
                         classCache.put(className, cls);
                         // Also cache full key
                         classCache.put(key, cls);
                         return cls;
-                    } catch (ClassNotFoundException ignore) {
+                    } catch (ClassNotFoundException ex) {
+                        if (null == fail)
+                            fail = ex;
+                        else
+                            fail.addSuppressed(ex);
                     }
+                }
+                throw new IllegalArgumentException(format("%s '%s' not found",
+                                                          label, className), fail);
             } else {
                 try {
                     cls = classLoader.loadClass(className);
                     if (!type.isAssignableFrom(cls)) {
-                        errorLog.accept(format("%s '%s' not an instanceof %s",
-                                               label, className, type.getTypeName()), null);
-                        cls = null;
+                        throw new IllegalArgumentException(format("%s '%s' not an instanceof %s",
+                                                                  label, className, type.getTypeName()));
                     }
                 } catch (ClassNotFoundException ex) {
-                    errorLog.accept(format("%s '%s' not found",
-                                           label, className), ex);
-                    cls = null;
+                    throw new IllegalArgumentException(format("%s '%s' not found", label, className), ex);
                 }
             }
-            classCache.put(className, cls);
-            if (supportedPackagePrefixes.contains(className.substring(p + 1)))
-                classCache.put(className.substring(p + 1), cls);
-            //
+            cacheClass(className, cls, p);
             return cls;
         }
     }
@@ -149,9 +157,8 @@ public class STContext implements Closeable {
         try {
             return (T) cls.getConstructor().newInstance();
         } catch (Exception ex) {
-            errorLog.accept(format("%s '%s' (%s) instance construction failed",
-                                   label, className, type.getTypeName()), null);
-            return null;
+            throw new IllegalStateException(format("%s '%s' (%s) instance construction failed",
+                                                   label, className, type.getTypeName()), ex);
         }
     }
 
@@ -160,14 +167,11 @@ public class STContext implements Closeable {
      *
      * @param packagePrefix .
      */
-    public void addPackagePrefix(String packagePrefix) {
-        packagePrefix = Objects.requireNonNull(packagePrefix, "packagePrefix");
+    public void addPackage(@NonNull String packagePrefix) {
         if (packagePrefix.isEmpty())
             throw new IllegalArgumentException("packagePrefix is blank");
-        if (packagePrefix.indexOf('.') == -1)
-            packagePrefix = packagePrefix.concat(".");
-        synchronized (supportedPackagePrefixes) {
-            supportedPackagePrefixes.add(packagePrefix);
+        synchronized (PACKAGE_NAMES) {
+            PACKAGE_NAMES.add(packagePrefix);
         }
     }
 
@@ -178,9 +182,7 @@ public class STContext implements Closeable {
                                  stGroup::registerModelAdaptor);
     }
 
-    public STMessage patch(STMessage msg, String encoding) {
-        Objects.requireNonNull(msg, "msg");
-        Objects.requireNonNull(encoding, "encoding");
+    public STMessage patch(@NonNull STMessage msg, @NonNull String encoding) {
         if (msg instanceof STRuntimeMessage) {
             final STRuntimeMessage strm = (STRuntimeMessage) msg;
             InstanceScope scope = strm.scope;
@@ -194,6 +196,7 @@ public class STContext implements Closeable {
                         return new STRuntimeMessagePatch((STRuntimeMessage) msg, n);
                     scope = scope.parent;
                 } while (scope != null);
+                //
                 scope = strm.scope;
                 final ST st = scope.st;
                 final String name = st.getName().substring(1);
@@ -235,9 +238,29 @@ public class STContext implements Closeable {
         }
     }
 
-    private static final Set<String> supportedPackagePrefixes =
-            new HashSet<>(Arrays.asList(
-                    "java.lang.",
-                    "java.util."
-                                       ));
+    private static final Set<String> PACKAGE_NAMES =
+            new LinkedHashSet<>(Arrays.asList(
+                    "java.lang",
+                    "java.util",
+                    "org.stringtemplate.v4",
+                    "rwperrott.stringtemplate.v4"
+                    ));
+    private static final Class<?>[] PRE_CACHED_CLASSES = {
+            // Possible attribute types
+            CharSequence.class,
+            String.class,
+            Number.class,
+            Collection.class,
+            List.class,
+            Map.class,
+            Set.class,
+            // Renderers
+            StringRenderer.class,
+            StringInvokeRenderer.class,
+            // Adapters
+            ObjectInvokeAdaptor.class,
+            NumberInvokeAdaptor.class,
+            StringInvokeAdaptor.class
+    };
+
 }
