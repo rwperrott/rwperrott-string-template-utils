55ffa84413f47fbc702025265bc1eb5ef5abe0a1
Richard Perrott <52632091+rwperrott@users.noreply.github.com>
2020-12-21 21:19:50 +0000

    Tweaked

--- src/main/java/rwperrott/stringtemplate/v4/SimplePool.java
+++ src/main/java/rwperrott/stringtemplate/v4/SimplePool.java
@@ -10,6 +10,7 @@ import java.util.function.Supplier;
 
 /**
  * No need for a pool library for simple memory-based stuff
+ *
  * @param <T>
  */
 public final class SimplePool<T> {
@@ -29,45 +30,61 @@ public final class SimplePool<T> {
         this.consumer = consumer;
     }
 
-    public T remove() {
-        final T t = pool.poll();
-        return null == t ? supplier.get() : t;
-   }
+    /**
+     * @param a can be null or contain null references, to allow for speculative use.
+     */
+    @SafeVarargs
+    public final void offer(T... a) {
+        if (null == a)
+            return;
+        int i = a.length;
+        while (--i >= 0)
+            offer(a[i]);
+    }
 
+    /**
+     * Recycles objects, then offers to pool.
+     * <p>
+     * If offer fails and consumer not null, is given to consumer to depose of it.
+     *
+     * @param t .
+     */
     public void offer(T t) {
         // Allow null, to support use of optional swap instance
         if (null == t)
             return;
         // Must recycle before offering!
         recycler.accept(t);
-        if (!pool.offer(t))
-            if (null != consumer)
-                consumer.accept(t);
-    }
-
-    @SuppressWarnings("unused")
-    @SafeVarargs
-    public final void offer(T... a) {
-        if (null == a)
-            return;
-        int i = a.length;
-        while (--i >=0)
-            offer(a[i]);
+        if (!pool.offer(t) && null != consumer)
+            consumer.accept(t);
     }
 
     /**
      * Make one-shot use easy
      *
-     * @param user .
-     * @param <R> .
-     * @return .
+     * @param user must not be null, accepts temporary resource and returns result
+     * @param <R> type of result
+     *
+     * @return result
      */
-    public <R> R use(Function<T,R> user) {
+    public <R> R use(@NonNull Function<T, R> user) {
         final T t = remove();
         try {
             return user.apply(t);
         } finally {
             offer(t);
         }
+    }
+
+    /**
+     * Only polls pool or creates a new object.
+     * <p>
+     * It's pointless doing any re-processing on a pool object, because it's already been recycled by offer.
+     *
+     * @return resource
+     */
+    public T remove() {
+        final T t = pool.poll();
+        return null == t ? supplier.get() : t;
     }
 }
