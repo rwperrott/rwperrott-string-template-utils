e0825b82322b9dfa72c9a6b00b6876da747e9ca4
Richard Perrott <52632091+rwperrott@users.noreply.github.com>
2022-04-18 18:45:22 +0100

    Split some methods to reduce Cyclomatic Complexity. Fixed missing invokers bug. Now only allow mapping of public instance Fields, public Methods, and public Constructors, to avoid issues when post Java 8 SDK used.

--- src/main/java/rwperrott/stringtemplate/v4/ClassMembers.java
+++ src/main/java/rwperrott/stringtemplate/v4/ClassMembers.java
@@ -15,6 +15,7 @@ import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.String.format;
 import static rwperrott.stringtemplate.v4.TypeConverter.box;
 
 /**
@@ -25,20 +26,6 @@ import static rwperrott.stringtemplate.v4.TypeConverter.box;
  * @author rwperrott
  */
 public final class ClassMembers {
-    public final Class<?> cls; // Included to that duplicates can be detected
-    private final Map<String, MemberInvokersImpl> instanceInvokers;
-    private final Map<String, MemberInvokersImpl> staticInvokers;
-
-    void addTo(TypeFunctions.FunctionsMap toMap) {
-        // Used to block adding of duplicate MemberInvokers.
-        final ObjectSet<MemberInvoker> unique = new ObjectOpenHashSet<>();
-        if (toMap.valueType() == cls) {
-            toMap.mergeInstanceInvokers(unique, instanceInvokers);
-        }
-        //
-        toMap.mergeStaticInvokers(unique, staticInvokers);
-    }
-
     /*
      * A cache to reduce the cost of subsequent lookups; even failures are cached.
      */
@@ -51,93 +38,33 @@ public final class ClassMembers {
             cache.put(cls, new ClassMembers(cls));
     }
 
-    public static ClassMembers of(final @NonNull Class<?> cls) {
-        synchronized (cache) {
-            return cache.computeIfAbsent(cls, ClassMembers::new);
-        }
-    }
+    public final Class<?> cls; // Included to that duplicates can be detected
+    private final Map<String, MemberInvokersImpl> instanceInvokers;
+    private final Map<String, MemberInvokersImpl> staticInvokers;
 
     private ClassMembers(final Class<?> cls) {
         this.cls = cls;
         final MethodHandles.Lookup lookup = MethodHandles.lookup();
         // Collect in name ordered Red Black maps.
-        final Map<String, MemberInvokersImpl> instanceInvokers_ = new Object2ObjectRBTreeMap<>();
-        final Map<String, MemberInvokersImpl> staticInvokers_ = new Object2ObjectRBTreeMap<>();
-        for (final Field f : cls.getDeclaredFields()) {
-            final MethodHandle mh;
-            try {
-                mh = lookup.unreflectGetter(f);
-            } catch (IllegalAccessException e) {
-                continue;
-            }
-            (Modifier.isStatic(f.getModifiers()) ? staticInvokers_ : instanceInvokers_)
-                    .computeIfAbsent(f.getName(), MemberInvokersImpl::new)
-                    .accept(MemberInvoker.forField(box(f.getType()), f, mh));
-        }
-        //
-        for (final Method m : cls.getDeclaredMethods()) {
-            try {
-                final Class<?> returnType = box(m.getReturnType());
-                if (returnType == Void.class)
-                    continue;
-                //
-                final boolean isStatic = Modifier.isStatic(m.getModifiers());
-                if (isStatic && 0 == m.getParameterCount())
-                    continue;
-                //
-                final Class<?>[] parameterTypes = m.getParameterTypes();
-                if (isStatic) {
-                    final TypeIndexMap valueIndexOf = new TypeIndexMap();
-                    final TypeConverter[] typeConverters =
-                            TypeConverter.toTypeConverters(parameterTypes, valueIndexOf);
-                    if (null != typeConverters) {
-                        final MethodHandle mh = lookup.unreflect(m);
-                        staticInvokers_
-                                .computeIfAbsent(m.getName(), MemberInvokersImpl::new)
-                                .accept(MemberInvoker.forStaticMethod(returnType, m, mh, typeConverters, valueIndexOf));
-                    }
-                } else {
-                    final TypeConverter[] typeConverters =
-                            TypeConverter.toTypeConverters(parameterTypes);
-                    if (null != typeConverters) {
-                        // Fix for match all, never convert, bug for equals(Object), which caused erroneous false results.
-                        if (typeConverters.length == 1 && parameterTypes[0] == Object.class && m.getName().equals("equals"))
-                            typeConverters[0] = TypeConverter.toTypeConverter(cls);
-                        final MethodHandle mh = lookup.unreflect(m);
-                        instanceInvokers_
-                                .computeIfAbsent(m.getName(), MemberInvokersImpl::new)
-                                .accept(MemberInvoker.forMethod(returnType, m, mh, typeConverters));
-                    }
-                }
-            } catch (IllegalAccessException ignore) {
-            }
-        }
-        //
-        final String simpleName = cls.getSimpleName();
-        for (final Constructor<?> c : cls.getDeclaredConstructors()) {
-            if (0 == c.getParameterCount())
-                continue;
-            try {
-                final TypeIndexMap valueIndexOf = new TypeIndexMap();
-                final TypeConverter[] typeConverters =
-                        TypeConverter.toTypeConverters(c.getParameterTypes(), valueIndexOf);
-                if (null != typeConverters) {
-                    final MethodHandle mh = lookup.unreflectConstructor(c);
-                    staticInvokers_
-                            .computeIfAbsent(simpleName, MemberInvokersImpl::new)
-                            .accept(MemberInvoker.forConstructor(cls, c, mh, typeConverters, valueIndexOf));
-                }
-            } catch (IllegalAccessException ignore) {
-            }
-        }
+        final Map<String, MemberInvokersImpl> staticInvokers0 = new Object2ObjectRBTreeMap<>();
+        final Map<String, MemberInvokersImpl> instanceInvokers0 = new Object2ObjectRBTreeMap<>();
+        acceptFields(lookup, instanceInvokers0);
+        acceptMethods(lookup, staticInvokers0, instanceInvokers0);
+        acceptConstructors(lookup, staticInvokers0);
 
         // Sort and index lists for ordered search sequence, which should help direct use and may speed-up indirect use.
-        staticInvokers_.values().forEach(MemberInvokersImpl::sort);
-        instanceInvokers_.values().forEach(MemberInvokersImpl::sort);
+        staticInvokers0.values().forEach(MemberInvokersImpl::sort);
+        instanceInvokers0.values().forEach(MemberInvokersImpl::sort);
 
         // Convert maps to linked hash maps, to retain name order, but with faster lookup.
-        this.instanceInvokers = new Object2ObjectLinkedOpenHashMap<>(instanceInvokers_);
-        this.staticInvokers = new Object2ObjectLinkedOpenHashMap<>(staticInvokers_);
+        this.instanceInvokers = new Object2ObjectLinkedOpenHashMap<>(instanceInvokers0);
+        this.staticInvokers = new Object2ObjectLinkedOpenHashMap<>(staticInvokers0);
+    }
+
+    public static ClassMembers of(final @NonNull Class<?> cls) {
+        synchronized (cache) {
+            return cache.computeIfAbsent(cls, ClassMembers::new);
+        }
     }
 
     // Used by TypeConverter and TypeIndexMap
@@ -147,5 +74,107 @@ public final class ClassMembers {
         if (type == from)
             return true;
         return !Modifier.isFinal(type.getModifiers()) && type.isAssignableFrom(from);
+    }
+
+    void addTo(TypeFunctions.FunctionsMap toMap) {
+        // Used to block adding of duplicate MemberInvokers.
+        final ObjectSet<MemberInvoker> unique = new ObjectOpenHashSet<>();
+        if (toMap.valueType() == cls) {
+            toMap.mergeInstanceInvokers(unique, instanceInvokers);
+        }
+        //
+        toMap.mergeStaticInvokers(unique, staticInvokers);
+    }
+
+    private void acceptFields(final MethodHandles.Lookup lookup,
+                              final Map<String, MemberInvokersImpl> instanceInvokers) {
+        for (final Field f : cls.getFields()) {
+            // Ignore static fields, because probably never going to be useful.
+            if (Modifier.isStatic(f.getModifiers()))
+                continue;
+            //
+            final MethodHandle mh;
+            try {
+                mh = lookup.unreflectGetter(f);
+            } catch (IllegalAccessException e) {
+                continue;
+            }
+            instanceInvokers
+                    .computeIfAbsent(f.getName(), MemberInvokersImpl::new)
+                    .accept(MemberInvoker.forField(box(f.getType()), f, mh));
+        }
+    }
+
+    private void acceptMethods(final MethodHandles.Lookup lookup,
+                               final Map<String, MemberInvokersImpl> staticInvokers0,
+                               final Map<String, MemberInvokersImpl> instanceInvokers0) {
+        for (final Method method : cls.getMethods()) {
+            try {
+                final Class<?> returnType = box(method.getReturnType());
+                if (returnType == Void.class)
+                    continue;
+                //
+                final Class<?>[] parameterTypes = method.getParameterTypes();
+                if (Modifier.isStatic(method.getModifiers())) {
+                    if (method.getParameterCount() != 0) // Must have at least one method for source type!
+                        acceptStaticMethod(lookup, staticInvokers0, method, returnType, parameterTypes);
+                } else {
+                    acceptInstanceMethod(lookup, instanceInvokers0, method, returnType, parameterTypes);
+                }
+            } catch (IllegalAccessException ignore) {
+            }
+        }
+    }
+
+    private void acceptStaticMethod(final MethodHandles.Lookup lookup,
+                                    final Map<String, MemberInvokersImpl> staticInvokers0,
+                                    final Method method,
+                                    final Class<?> returnType,
+                                    final Class<?>[] parameterTypes) throws IllegalAccessException {
+        final TypeIndexMap valueIndexOf = new TypeIndexMap();
+        final TypeConverter[] typeConverters = TypeConverter.toTypeConverters(parameterTypes, valueIndexOf);
+        if (null != typeConverters) {
+            staticInvokers0
+                    .computeIfAbsent(method.getName(), MemberInvokersImpl::new)
+                    .accept(MemberInvoker.forStaticMethod(returnType, method, lookup.unreflect(method), typeConverters, valueIndexOf));
+        }
+    }
+
+    private void acceptInstanceMethod(final MethodHandles.Lookup lookup,
+                                      final Map<String, MemberInvokersImpl> instanceInvokers0,
+                                      final Method method,
+                                      final Class<?> returnType,
+                                      final Class<?>[] parameterTypes) throws IllegalAccessException {
+        final TypeConverter[] typeConverters = TypeConverter.toTypeConverters(parameterTypes);
+        if (null != typeConverters) {
+            // Fix for match all, never convert, bug for equals(Object), which caused erroneous false results.
+            if (typeConverters.length == 1 && parameterTypes[0] == Object.class && method.getName().equals("equals"))
+                typeConverters[0] = TypeConverter.toTypeConverter(cls);
+            instanceInvokers0
+                    .computeIfAbsent(method.getName(), MemberInvokersImpl::new)
+                    .accept(MemberInvoker.forMethod(returnType, method, lookup.unreflect(method), typeConverters));
+        }
+    }
+
+    private void acceptConstructors(final MethodHandles.Lookup lookup,
+                                    final Map<String, MemberInvokersImpl> staticInvokers) {
+        final String simpleName = cls.getSimpleName();
+        for (final Constructor<?> c : cls.getConstructors()) {
+            if (0 == c.getParameterCount())
+                continue;
+            //
+            try {
+                final TypeIndexMap valueIndexOf = new TypeIndexMap();
+                final TypeConverter[] typeConverters = TypeConverter.toTypeConverters(c.getParameterTypes(), valueIndexOf);
+                if (null != typeConverters) {
+                    final MethodHandle mh = lookup.unreflectConstructor(c);
+                    staticInvokers
+                            .computeIfAbsent(simpleName, MemberInvokersImpl::new)
+                            .accept(MemberInvoker.forConstructor(cls, c, mh, typeConverters, valueIndexOf));
+                }
+            } catch (IllegalAccessException ignore) {
+            }
+        }
+
     }
 }
