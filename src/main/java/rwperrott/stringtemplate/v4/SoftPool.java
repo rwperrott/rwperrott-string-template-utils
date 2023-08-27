ee0f2a59f256b7579f8fa7c3d30bcdc3c9e22626
Richard Perrott <52632091+rwperrott@users.noreply.github.com>
2023-08-27 09:44:29 +0100

    Redesigned SoftPool, to get rid of some glaring design mistakes.

--- src/main/java/rwperrott/stringtemplate/v4/SoftPool.java
+++ src/main/java/rwperrott/stringtemplate/v4/SoftPool.java
@@ -1,242 +1,299 @@
 package rwperrott.stringtemplate.v4;
 
-import lombok.NonNull;
+import lombok.*;
+import lombok.experimental.Accessors;
 
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 /**
- * Based upon LinkedBlockingQueue, commons-pool PooledObjectFactory, and WeakHashMap.
+ * A Thread-safe soft-reference-based pooling class using a concurrent queue,
+ * and functional references, for a far less bloaded pool implementation than commons-pool2.
+ * <br/>
+ * Loosely based upon LinkedBlockingQueue, commons-pool2 PooledObjectFactory, and WeakHashMap.
  *
  * @param <E> resource type.
  * @author rwperrott
  */
-@SuppressWarnings("unused")
-public final class SoftPool<E> implements Supplier<E>, UnaryOperator<E>, Consumer<E> {
-    private final int capacity;
-    private final AtomicInteger count = new AtomicInteger();
-    private final Supplier<E> supplier;
-    private final Predicate<E> passivator;
-    private final Predicate<E> activator;
-    private final Consumer<E> destroyer;
-    private final ReferenceQueue<E> refQueue;
-    private final ReentrantLock getLock = new ReentrantLock();
-    private final ReentrantLock acceptLock = new ReentrantLock();
-    private Node<E> head;
-    private Node<E> last;
+@SuppressWarnings({"unused", "SpellCheckingInspection"})
+public final class SoftPool<E> implements CloseableSupplier<CloseableSupplier<E>> {
+  /**
+   * @param <E> resource type.
+   */
+  public static <E> Builder<E> builder(@NonNull Supplier<E> supplier) {
+    return new Builder<>(supplier);
+  }
 
-    private SoftPool(int capacity,
-                     final Supplier<E> supplier,
-                     final Predicate<E> passivator,
-                     final Predicate<E> activator,
-                     final Consumer<E> destroyer) {
-        if (capacity <= 0) throw new IllegalArgumentException();
-        this.capacity = capacity;
-        this.supplier = supplier;
-        this.passivator = passivator;
-        this.activator = activator;
-        this.destroyer = destroyer;
-        last = head = new Node<>(null);
-        refQueue = null == activator
-                ? null
-                : new ReferenceQueue<>();
-    }
+  /**
+   * Reset state of instance, e.g. StringBuilder.setLength(0).
+   */
+  private final Consumer<E> passivator; // For pool
 
-    /**
-     * Do like accept(e), get(), to get or reuse a resource
-     * <p>
-     * Useful for clear-able builder objects, like StringBuilder.
-     *
-     * @param e null or resource
-     * @return resource
-     */
-    @Override
-    public E apply(final E e) {
-        if (null != e
-                && (null == passivator || passivator.test(e))
-                && (null == activator || activator.test(e)))
-            return e;
-        return get();
-    }
+  /**
+   * Check if instance usable.
+   */
+  private final Predicate<E> validator; // For pool
 
-    /**
-     * Provide resource
-     *
-     * @return resource
-     */
-    public E get() {
-        cleanup();
+  /**
+   * Destroy instance.
+   */
+  private final Consumer<E> destroyer; // For pool
 
-        final AtomicInteger count0 = this.count;
-        if (count0.get() == 0)
-            return supplier.get();
-        //
-        final ReentrantLock getLock0 = this.getLock;
-        getLock0.lock();
+  private final AtomicInteger count = new AtomicInteger(); // For queue
+  private final ReentrantLock pollLock = new ReentrantLock(); // For queue
+  private final ReentrantLock offerLock = new ReentrantLock(); // For queue
+  /**
+   * Queue of stale instances, which must be destroyed, and left for GC.
+   */
+  private final ReferenceQueue<E> referenceQueue; // For SoftReference Nodes
+
+  /**
+   * The maximum pool capacity.
+   */
+  private int capacity;
+  private Node<E> head; // For queue
+  private Node<E> last; // For queue
+
+  /**
+   * Creates new instances of E.
+   */
+  private Supplier<E> supplier;
+
+  private SoftPool(int capacity,
+                   Supplier<E> supplier,
+                   Consumer<E> passivator,
+                   Predicate<E> validator,
+                   Consumer<E> destroyer,
+                   ReferenceQueue<E> staleReferences) {
+    this.capacity = capacity;
+    this.supplier = supplier;
+    this.passivator = passivator;
+    this.validator = validator;
+    this.destroyer = destroyer;
+    this.referenceQueue = staleReferences;
+    last = head = new Node<>(null);
+  }
+
+  private void offer(E e) {
+    cleanup();
+
+    boolean accepted = false;
+    try {
+      final AtomicInteger count = this.count;
+      if (validator.test(e) && count.get() < capacity) { // not full
+        final ReentrantLock offerLock = this.offerLock;
+        offerLock.lock();
         try {
-            // Look for a non-null SoftReference value.
-            while (count0.get() > 0) {
-                //dequeue
-                final Node<E> h = head;
-                final Node<E> first = h.nextNode;
-                h.nextNode = h; // help GC
-                head = first;
-                final E x = first.get();
-                first.clear();
-                count0.getAndDecrement();
-                if (null != x) {
-                    if (null == activator || activator.test(x))
-                        return x;
-                    if (null != destroyer)
-                        destroyer.accept(x);
-                }
-            }
+          if (count.get() >= capacity) return; // full
+          // No point calling passivator until we know it will be stored.
+          passivator.accept(e);
+          // enqueue like in LinkedBlocking Queue
+          last = last.nextNode = new Node<>(e);
+          count.getAndIncrement();
+          accepted = true;
         } finally {
-            getLock0.unlock();
+          offerLock.unlock();
         }
-        return supplier.get();
+      }
+    } finally {
+      if (!accepted) destroyer.accept(e);
+    }
+  }
+
+  private E poll() {
+    cleanup();
+
+    final AtomicInteger count = this.count;
+    if (count.get() != 0) {
+      final ReentrantLock pollLock = this.pollLock;
+      pollLock.lock();
+      try {
+        while (count.get() != 0) {
+          // dequeue like in LinkedBlocking Queue
+          final Node<E> h = head;
+          final Node<E> first = h.nextNode; // .next is for ReferenceQueue use
+          h.nextNode = h; // help GC
+          head = first;
+          final E e = first.get();
+          if (e != null) {
+            first.clear();
+            // An activator only seem useful, to update use state after validation.
+            // So doesn't really look useful.
+            if (validator.test(e)) return e;
+            destroyer.accept(e);
+          }
+          count.getAndDecrement();
+        }
+      } finally {
+        pollLock.unlock();
+      }
     }
 
-    /**
-     * Could be called by a regular cleanup task.
-     */
-    public void cleanup() {
-        getLock.lock();
-        try {
-            if (null != refQueue)
-                for (Reference<? extends E> x; (x = refQueue.poll()) != null; ) {
-                    final E e = x.get();
-                    if (null == e)
-                        continue;
-                    destroyer.accept(x.get());
-                    x.clear();
-                }
-        } finally {
-            getLock.unlock();
-        }
-    }
+    return supplier.get();
+  }
 
-    /**
-     * Allow some code to temporarily use resource.
-     * <p>
-     * Useful for clear-able builder objects, like StringBuilder.
-     *
-     * @param user .
-     */
-    public <R> R use(final Function<E, R> user) {
-        final E e = get();
-        try {
-            return user.apply(e);
-        } finally {
-            accept(e);
-        }
-    }
+  /**
+   * Get a valid instance of E from pool or supplier, wrapped in a CloseableSupplier.
+   *
+   * @return resource.
+   */
+  public CloseableSupplier<E> get() {
+    return new Borrowed(poll());
+  }
 
-    /**
-     * Accept returned resource, and keep or depose of it.
-     *
-     * @param e resource
-     */
-    public void accept(E e) {
+  /**
+   * Main call to clean up all the expired resource soft references.
+   * Thread-safe to call by external code.
+   */
+  public void cleanup() {
+    if (null == referenceQueue)
+      return;
+    pollLock.lock();
+    try {
+      for (Reference<? extends E> x; (x = referenceQueue.poll()) != null; ) {
+        final E e = x.get();
         if (null == e)
-            return;
-        //
-        if (null != passivator && !passivator.test(e)) {
-            if (null != destroyer)
-                destroyer.accept(e);
-            return;
-        }
-        //
-        final AtomicInteger count0 = this.count;
-        if (count0.get() == capacity)
-            return;
-        //
-        final ReentrantLock acceptLock0 = this.acceptLock;
-        acceptLock0.lock();
-        try {
-            if (count0.get() < capacity)
-                last = last.nextNode = null == refQueue
-                        ? new Node<>(e)
-                        : new Node<>(e, refQueue);
-        } finally {
-            acceptLock0.unlock();
-        }
+          continue;
+        destroyer.accept(e);
+        x.clear();
+      }
+    } finally {
+      pollLock.unlock();
+    }
+  }
+
+  /**
+   * Dispose of all the elements in reference queue and pool.
+   */
+  public void close() {
+    offerLock.lock();
+    pollLock.lock();
+    try {
+      // Block new creation
+      supplier = null;
+
+      // Drop all returns
+      capacity = 0;
+
+      // Drop all returned
+      for (Node<E> p, h = head; (p = h.nextNode) != null; h = p) {
+        h.nextNode = h;
+        p.clear();
+      }
+      head = last;
+    } finally {
+      pollLock.unlock();
+      offerLock.unlock();
+    }
+    cleanup();
+  }
+
+  /**
+   * SoftReference extended as a single linked-list node for the pool queue.
+   *
+   * @param <E>
+   */
+  private static final class Node<E> extends SoftReference<E> {
+    private Node<E> nextNode;
+
+    private Node(E x) {
+      super(x);
+    }
+  }
+
+  /**
+   * Sadly the Project Lombok @Builder annotation does not provide enough functionality,
+   * like validation and initialisation, so I had to create this class.
+   *
+   * @param <E> the interface/object type pooled.
+   */
+  @Accessors(fluent = true)
+  public static final class Builder<E> {
+    private static <E> Consumer<E> resolve(Consumer<E> o) {
+      return null == o ? e -> {
+      } : o;
+    }
+
+    private static <E> Predicate<E> resolve(Predicate<E> o) {
+      return null == o ? e -> true : o;
     }
 
     /**
-     * Atomically depose of all the elements in this pool.
+     * The supplier for new instances of E, the pooled resource.
      */
-    public void clear() {
-        acceptLock.lock();
-        getLock.lock();
-        try {
-            for (Node<E> p, h = head; (p = h.nextNode) != null; h = p) {
-                h.nextNode = h;
-                p.clear();
-            }
-            head = last;
-        } finally {
-            getLock.unlock();
-            acceptLock.unlock();
-        }
+    @Getter
+    private final Supplier<E> supplier;
+    /**
+     * The maximum pool capacity.
+     */
+    @Setter
+    private int capacity = Integer.MAX_VALUE;
+    /**
+     * Converts an offered E into a passive state, and returns true if successful.
+     * If true, it will be pushed onto the pool queue.
+     */
+    @Setter
+    @NonNull
+    private Consumer<E> passivator = e -> {
+    };
+
+    @Setter
+    private Predicate<E> validator;
+
+    private Consumer<E> destructorStub;
+    @Setter
+    private Consumer<E> destroyer;
+
+    @Setter
+    private boolean useReferenceQueue;
+
+    /**
+     * @param supplier creates new instances of E for the pool.
+     */
+    private Builder(@NonNull Supplier<E> supplier) {
+      this.supplier = supplier;
     }
 
-    // SoftReference extended as a single linked-list node.
-    static final class Node<E> extends SoftReference<E> {
-        Node<E> nextNode;
+    public SoftPool<E> build() {
+      if (capacity <= 0) throw new IllegalArgumentException("" + capacity);
+      return new SoftPool<>(capacity,
+                            supplier,
+                            resolve(passivator),
+                            resolve(validator),
+                            resolve(destroyer),
+                            useReferenceQueue ? new ReferenceQueue<>() : null);
+    }
+  }
 
-        Node(E x) {
-            super(x);
-        }
+  /**
+   * This cannot be the same object as Node, to prevent memory leaks!
+   */
+  @AllArgsConstructor(access = AccessLevel.PRIVATE)
+  private final class Borrowed implements CloseableSupplier<E> {
+    private E ref;
 
-        Node(E x, ReferenceQueue<? super E> q) {
-            super(x, q);
-        }
+    @Override
+    public E get() {
+      return ref;
     }
 
-    @SuppressWarnings("unused")
-    public static final class Builder<E> {
-        private final Supplier<E> supplier;
-        private int capacity = Integer.MAX_VALUE;
-        private Predicate<E> passivator;
-        private Predicate<E> activator;
-        private Consumer<E> destroyer;
-
-        private Builder(@NonNull final Supplier<E> supplier) {
-            this.supplier = supplier;
-        }
-
-        public Builder<E> capacity(final int capacity) {
-            this.capacity = capacity;
-            return this;
-        }
-
-        public Builder<E> passivator(@NonNull final Predicate<E> passivator) {
-            this.passivator = passivator;
-            return this;
-        }
-
-        public Builder<E> activator(@NonNull final Predicate<E> activator) {
-            this.activator = activator;
-            return this;
-        }
-
-        public Builder<E> destroyer(@NonNull final Consumer<E> destroyer) {
-            this.destroyer = destroyer;
-            return this;
-        }
-
-        public SoftPool<E> build() {
-            if (capacity <= 0) throw new IllegalArgumentException("capacity: " + capacity);
-            return new SoftPool<>(capacity, supplier, passivator, activator, destroyer);
-        }
+    private E getAndClear() {
+      E r = ref;
+      ref = null;
+      return r;
     }
 
-    public static <E> Builder<E> builder(@NonNull Supplier<E> supplier) {
-        return new Builder<>(supplier);
+    @Override
+    public void close() {
+      final E e = this.ref;
+      this.ref = null;
+      offer(e);
     }
+  }
 }
